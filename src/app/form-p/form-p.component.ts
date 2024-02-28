71058ad60f2b2672982b88f54d20dbb4cf2eb31e
augustobrigo <profeaugustobriga@gmail>
2024-02-28 09:14:05 +0100

    Commit ChatGPTPachon

--- src/app/form-p/form-p.component.ts
+++ src/app/form-p/form-p.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-p',
   templateUrl: './form-p.component.html',
   styleUrls: ['./form-p.component.css']
 })
-export class FormPComponent {
+export class FormPComponent implements OnInit{
 miform!: FormGroup;
+constructor(private fb:FormBuilder){}
+  ngOnInit(): void {
+    this.miform=this.fb.group({
+      email:['',[Validators.required,Validators.email]],
+      fecha:['',[Validators.required]],
+      trabajo:['',[Validators.required]],
+      ps:['', [Validators.required, Validators.pattern('^[A-Z].*[0-9]$'), Validators.minLength(10), Validators.maxLength(10)]],
+    })
+  }
 
 
 }
