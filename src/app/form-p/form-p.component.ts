94661a3441d8bb4e1f02aba06a4c21f9e999d947
augustobrigo <profeaugustobriga@gmail>
2024-02-28 08:48:57 +0100

    Formulario Reactivo

---
+++ src/app/form-p/form-p.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-p',
+  templateUrl: './form-p.component.html',
+  styleUrls: ['./form-p.component.css']
+})
+export class FormPComponent {
+miform!: FormGroup;
+
+}
